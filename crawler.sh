6f8eedab76b8e29173207f98126d0be1e16918ce
uzohou <isaiahntina47@gmail.com>
2024-01-10 16:35:13 +0000

    Refactor loading_animation function in crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -3,17 +3,17 @@
 # Set the search path to the user's desktop directory
 search_path="$HOME/desktop"
 
-loading_animation(){
-	local pid = $1
-	local spin = '-\|/'
-	local i=0
-	while kill -0 "$pid" 2>/dev/null; do
-		i=$(((i+1)%4))
-		printf "\rDeleting node_modules... [%c]" "${spin:$i:1}"
-		sleep 0.1
-	done
-	printf "\rDeleting node_modules... [Done]     \n"
-	}
+loading_animation() {
+    local pid= $1
+    local spin= '-\|/'
+    local i= 0
+    while kill -0 "$pid" 2>/dev/null; do
+        i=$(( (i+1) % 4 ))
+        printf "\rDeleting node_modules... [%c] " "${spin:$i:1}"
+        sleep 0.1
+    done
+    printf "\rDeleting node_modules... [Done]     \n"
+}
 
 # Find and remove all node_modules directories within the specified path
 find "$search_path" -name "node_modules" -type d -print0 | 
